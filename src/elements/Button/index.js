885ba8e01ec9a17cc5fb09f46f16d5f84afc191c
prasagungg <agungprasetio878@gmail.com>
2021-10-11 11:13:40 +0700

    Hero in landing page

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -44,7 +44,7 @@ export default function Button(props) {
           href={props.href}
           className={className.join(" ")}
           style={props.style}
-          target={props.targe === "_blank" ? "_blank" : undefined}
+          target={props.target === "_blank" ? "_blank" : undefined}
           rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
         >
           {props.children}
